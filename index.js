c6f44d6fb63158cd5928e48d24fe7fb69756f414
santiagobenfatto <santiagobenfatto@outlook.com>
2024-07-09 12:39:28 -0300

    Update File Metadata project

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
 
-var app = express();
+const app = express()
 
+//Multer configs
+//const upload = multer({ dest: 'uploads/' })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+
+
+//Middlewares
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html')
 });
 
 
+//Multer implementation
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  ///name, type and size.
+  const data = {
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  }
+
+  res.json(data)
+})
 
 
 const port = process.env.PORT || 3000;
